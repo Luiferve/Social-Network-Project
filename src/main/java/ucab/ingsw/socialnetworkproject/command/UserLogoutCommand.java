691ed56250851f772fd929959563093b65301945
Daniel Rojas <danired18@gmail.com>
2018-06-03 00:06:37 -0400

    Fixes to the token verification

--- src/main/java/ucab/ingsw/socialnetworkproject/command/UserLogoutCommand.java
+++ src/main/java/ucab/ingsw/socialnetworkproject/command/UserLogoutCommand.java
@@ -13,13 +13,11 @@ import java.io.Serializable;
 //Comando que se recibe para el cierre de sesion
 @ToString
 @Data
-public class UserLogoutCommand implements Serializable{
+public class UserLogoutCommand implements Serializable {
 
-    @NotNull(message = "Se requiere direccion de email.")
-    @NotEmpty(message = "Se requiere direccion de email.")
-    @Size(min = validationRules.EMAIL_MIN_SIZE, message = "El email debe contener al menos de 8 caracteres.")
-    @Email(message = "error.format.email")
-    private String email;
+    @NotNull(message = "Se requiere id del usuario.")
+    @NotEmpty(message = "Se requiere id del usuario.")
+    private String id;
 
     @NotNull(message = "Se requiere token de autorizacion.")
     @NotEmpty(message = "Se requiere token de autorizacion.")
