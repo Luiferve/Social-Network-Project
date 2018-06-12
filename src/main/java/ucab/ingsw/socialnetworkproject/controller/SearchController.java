aa6e24d8b9a0307c0ba820a1c9b110541271cd87
Daniel Rojas <danired18@gmail.com>
2018-06-11 21:28:44 -0400

    Added Special Characters Validation for instagram search

--- src/main/java/ucab/ingsw/socialnetworkproject/controller/SearchController.java
+++ src/main/java/ucab/ingsw/socialnetworkproject/controller/SearchController.java
@@ -3,12 +3,9 @@ package ucab.ingsw.socialnetworkproject.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriUtils;
-import ucab.ingsw.socialnetworkproject.response.AlertResponse;
+import ucab.ingsw.socialnetworkproject.service.Builder;
 import ucab.ingsw.socialnetworkproject.service.strategy.InstagramSearchStrategy;
 import ucab.ingsw.socialnetworkproject.service.SearchService;
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,14 +17,10 @@ public class SearchController {
     @Autowired
     private SearchService searchService;
 
-    private List<String> validStrategy;
+    @Autowired
+    private Builder builder;
 
-    private AlertResponse buildAlertResponse(String message){
-        AlertResponse response = new AlertResponse();
-        response.setMessage(message);
-        response.setTimestamp(LocalDateTime.now());
-        return response;
-    }
+    private List<String> validStrategy;
 
     private void setValidStrategy(){
         validStrategy = new ArrayList<>();
@@ -47,16 +40,22 @@ public class SearchController {
 
     @RequestMapping(value = "/{strategy}", method = RequestMethod.GET)
     public ResponseEntity search(@PathVariable("strategy") String strategy, @RequestParam("q") String searchTerm) {
-        boolean result = checkStrategy(strategy);
-        if(result) {
-            if (strategy.toLowerCase().equals("instagram")) {
+        if(checkStrategy(strategy)) {
+            if (strategy.toLowerCase().equals(validStrategy.get(0))) {
                 searchService.setSearchStrategy(new InstagramSearchStrategy());
+
+            } /*
+            else if (strategy.toLowerCase().equals(validStrategy.get(1))){
+                //searchService.setSearchStrategy(new YoutubeSearchStrategy());
             }
+            else if (strategy.toLowerCase().equals(validStrategy.get(2))){
+                //searchService.setSearchStrategy(new SoundcloudSearchStrategy());
+            }*/
             searchTerm = searchTerm.replace(" ", "");
             return searchService.search(searchTerm);
         }
         else{
-            return ResponseEntity.badRequest().body(buildAlertResponse("invalid_search_strategy"));
+            return ResponseEntity.badRequest().body(builder.buildAlertResponse("invalid_search_strategy"));
         }
     }
 
--- src/main/java/ucab/ingsw/socialnetworkproject/service/SearchService.java
+++ src/main/java/ucab/ingsw/socialnetworkproject/service/SearchService.java
@@ -1,11 +1,18 @@
 package ucab.ingsw.socialnetworkproject.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import ucab.ingsw.socialnetworkproject.service.strategy.SearchStrategy;
 
+@Slf4j
+
 @Service("searchService")
 public class SearchService {
+
+    @Autowired
+    private Builder builder;
 
     private SearchStrategy strategy;
 
@@ -13,7 +20,11 @@ public class SearchService {
         this.strategy = strategy;
     }
 
-    public ResponseEntity<Object> search(String searhTerm){
-        return  strategy.seeker(searhTerm);
+    public ResponseEntity<Object> search(String searchTerm){
+        if(searchTerm.matches("[a-zA-Z0-9]*"))
+            return  strategy.seeker(searchTerm);
+        else
+            log.info("Search term ={} contains invalid characters", searchTerm);
+            return ResponseEntity.badRequest().body(builder.buildAlertResponse("invalid_characters_in_search_term"));
     }
 }
